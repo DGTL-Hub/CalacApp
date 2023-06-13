2a80b59ba9b092fe7d2efba7b5ed00bb28669726
Svyatoslav Russkiy <russkiy1389@GMAIL.COM>
2023-06-13 13:34:11 -0800

    main: Add the math opeations and user input proccessing.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 /**
- * @file index.js@mod
- * @module index. 
+ * @file index.js
+ * @module index
  * @description A small command line calc application.
+ * @requires myMath
+ * @requires prompt
  * @requires {@link hhtps://www.npmjs.com/package/path|path}
  * @author Svyatoslav Oleg Russkiy
  * @date 2023/06/09
  * @copyright Copyright @ 2023-06-09 by Svyatoslav Oleg Russkiy all rights reserved.
  */
 
+// Internal imports
+let myMath = require('./myMath');
+let prompt = require('./prompt');
+// External imports
 let path = require('path');
 global.appRoot = path.resolve(process.cwd());
 let rootPath = '';
 let baseFileName = path.basename(module.filename, path.extname(module.filename));
-let namespacePrefix = `application.${baseFileName}`;
+let namespacePrefix = `application.${baseFileName}.`;
 
 /**
  * @function application
@@ -21,18 +27,16 @@ let namespacePrefix = `application.${baseFileName}`;
  * @author Svyatoslav Oleg Russkiy
  * @date 2023/06/09
  */
-
-
-// Internal imports
-let prompt = require('./prompt');
-// External imports
-
 function application() {
     let functionName = application.name;
     let argumentDriveInterface = false;
     let commandInput, commandResult;
-    let inputData1 = 0;
-    let inputData2 = 0;
+    // let inputDataUser1 = 0;
+    // let inputDataUser2 = 0;
+    
+    let inputDataValue1 = 0;
+    let inputDataValue2 = 0;
+    // let validInputString = false;
     console.log(`BEGIN ${namespacePrefix}${functionName} function`);
     console.log(`BEGIN main program loop`);
     console.log(`BEGIN command parser`);
@@ -42,19 +46,66 @@ function application() {
 
             if (commandInput) {
                 if (commandInput.toUpperCase().trim() === 'EXIT' ||
-                commandInput.toUpperCase().trim() === 'QUIT' ||
-                commandInput.toUpperCase().trim() === 'Q' ||
-                commandInput.toUpperCase().trim() === 'X') {
+                    commandInput.toUpperCase().trim() === 'QUIT' ||
+                    commandInput.toUpperCase().trim() === 'Q' ||
+                    commandInput.toUpperCase().trim() === 'X') {
                     console.log(`END command parser`);
                     programRunning = false;
                     console.log(`END main program loop`);
                     console.log("Exiting program.");
+                } else if (commandInput.toUpperCase().trim() === 'ADD') {
+                    // inputDataUser1 = "";
+                    // inputDataUser2 = "";
+                    // while (!validInputString) {
+                    //     inputDataUser1 = prompt.prompt(`Enter first number: `);
+                    //     if (!isNaN(parseInt(inputDataUser1))) {
+                    //         validInputString = true;
+                    //         inputDataValue1 = parseInt(inputDataUser1);
+                    //     }
+                    // }
+                    
+                    inputDataValue1 = 0;
+                    inputDataValue2 = 0;
+                    inputDataValue1 = getUserInput(`Enter second number to add: `);
+                    // validInputString = false;
+                    
+                    inputDataValue2 = getUserInput(`Enter second number: `);
+                    let addResult = myMath.add(inputDataValue1, inputDataValue2);
+                    console.log(`The result of adding ${inputDataValue1} and ${inputDataValue2} is: ${addResult}`);
                 }
             }
         }
     }
     console.log(`END ${namespacePrefix}${functionName} function`);
 }
+/**
+ * @function getUserInput
+ * @description gets user input from the user and validates that it is an int.
+ * @param {string} message the message to query the user for input.
+ * @returns {integer} An Int value from the user input. 
+ * @author  Svyatoslav Oleg Russkiy
+ * @date 2023/06/09
+ */
+function getUserInput(message) {
+    let functionName = getUserInput.name;
+    console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+    console.log(`message is: ${message}`);
+    let returnData = 0;
+    let inputData;
+    let validInputString = false;
+    while (!validInputString) {
+        inputData = prompt.prompt(message);
+        if (!isNaN(parseInt(inputData))) {
+            validInputString = true;
+            returnData = parseInt(inputData);
+        }
+    }
+    console.log(`returnData is: ${returnData}`);
+    console.log(`END ${namespacePrefix}${functionName} function`);
+    return returnData;
+}
+
+
 
 // excuse
 let programRunning = false;
---
+++ src/myMath.js
@@ -0,0 +1,132 @@
+/**
+ * @file myMath.js
+ * @module myMath
+ * @description A small command line calc application.
+ * @requires {@link hhtps://www.npmjs.com/package/path|path}
+ * @author Svyatoslav Oleg Russkiy
+ * @date 2023/06/13
+ * @copyright Copyright @ 2023-06-09 by Svyatoslav Oleg Russkiy all rights reserved.
+ */
+
+//Internal imports 
+
+//External imports
+let path = require('path');
+let baseFileName = path.basename(module.filename, path.extname(module.filename));
+let namespacePrefix = `application.${baseFileName}.`;
+
+/**
+ * @function add
+ * @description adds two numbers together and return value.
+ * @param {integer} data1 first number to add.
+ * @param {integer} data2 the second number to add.
+ * @returns {integer} returns the sum of the two numbers.
+ * @author Svyatoslav Oleg Russkiy
+ * @date 2023/06/13
+ */
+function add(data1, data2) {
+    let functionName = add.name;
+    console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+    console.log(`data1 is: ${JSON.stringify(data1)}`);
+    console.log(`data2 is: ${JSON.stringify(data2)}`);
+    let returnData = 0;
+    if (data1 && data2) {
+        returnData = data1 + data2;
+    }
+    console.log(`returnData is: ${returnData}`);
+    console.log(`END ${namespacePrefix}${functionName} function`);
+    return returnData;
+}
+/**
+ * @function subtract
+ * @description subtracts two numbers together and return value.
+ * @param {integer} data1 
+ * @param {integer} data2
+ * @returns {integer} returns the difference of the two numbers.
+ * @author Svyatoslav Oleg Russkiy
+ * @date 2023/06/13 
+ */
+function subtract(data1, data2) {
+    let functionName = subtract.name;
+    console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+    console.log(`data1 is: ${JSON.stringify(data1)}`);
+    console.log(`data2 is: ${JSON.stringify(data2)}`);
+    let returnData = 0;
+    if (data1 && data2) {
+        returnData = data1 - data2;
+    }
+    console.log(`returnData is: ${returnData}`);
+    console.log(`END ${namespacePrefix}${functionName} function`);
+    return returnData;
+}
+/**
+ * @function multiply
+ * @description multiplies two numbers together and return value.
+ * @param {integer} data1 
+ * @param {integer} data2 
+ * @return {integer} returns the product of the two numbers.
+ * @author Svyatoslav Oleg Russkiy
+ * @date 2023/06/13
+ */
+function multiply(data1, data2) {
+    let functionName = multiply.name;
+    console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+    console.log(`data1 is: ${JSON.stringify(data1)}`);
+    console.log(`data2 is: ${JSON.stringify(data2)}`);
+    let returnData = 0;
+    if (data1 && data2) {
+        returnData = data1 * data2;
+    }
+    console.log(`returnData is: ${returnData}`);
+    console.log(`END ${namespacePrefix}${functionName} function`);
+    return returnData;
+}
+/**
+ * @function divide 
+ * @param {integer} data1 The first number to divide.
+ * @param {integer} data2 The second number to divide.
+ * @returns {integer} returns the quotient of the two numbers. 
+ */
+function divide(data1, data2) {
+    let functionName = divide.name;
+    console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+    console.log(`data1 is: ${JSON.stringify(data1)}`);
+    console.log(`data2 is: ${JSON.stringify(data2)}`);
+    let returnData = 0;
+    if (data1 && data2) {
+        returnData = data1 / data2;
+    }
+    console.log(`returnData is: ${returnData}`);
+    console.log(`END ${namespacePrefix}${functionName} function`);
+    return returnData;
+}
+/**
+ * @function factorial
+ * @description raises a number to a power.
+ * @param {integer} data the number to factorialize.
+ * @returns {integer} returns the factorial of the number.
+ * @author Svyatoslav Oleg Russkiy
+ * @date 2023/06/13
+ */
+function factorial(data) {
+    let functionName = factorial.name;
+    console.log(`BEGIN ${namespacePrefix}${functionName} function`);
+    console.log(`data is: ${JSON.stringify(data)}`);
+    let returnData = 0;
+    if (data === 0) {
+        returnData = 1;
+    } else {
+        returnData = data * factorial(data - 1);
+    }
+    console.log(`returnData is: ${returnData}`);
+    console.log(`END ${namespacePrefix}${functionName} function`);
+    return returnData;
+}
+
+module.exports = {
+    ['add']: (data1, data2) => add(data1, data2),
+    ['subtract']: (data1, data2) => subtract(data1, data2),
+    ['multiply']: (data1, data2) => multiply(data1, data2),
+    ['divide']: (data1, data2) => divide(data1, data2),
+    ['factorial']: (data) => factorial(data)
+}
